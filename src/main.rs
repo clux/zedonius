9b7b2de65e64a1119dd8ac5c483074e30e0854e5
clux <analsandblaster@gmail.com>
2016-03-02 21:59:06 +0000

    use experimental version of hiirc with Arc wrapped Irc instance

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+#![allow(unused_variables)]
 extern crate hiirc;
 extern crate time;
+use std::thread;
+use std::sync::Arc;
 
 use hiirc::*;
 use time::Duration;
 
 struct Zedonius {
-  blah: bool,
+  blah: u32,
 }
 
 const NICKNAME: &'static str = "zedonius";
@@ -13,21 +16,27 @@ const USERNAME: &'static str = "hiirc";
 const REALNAME: &'static str = "Ruler of Flame";
 
 impl Listener for Zedonius {
-    fn any(&mut self, irc: &Irc, event: &Event) {
+    fn any(&mut self, irc: Arc<Irc>, event: &Event) {
         println!("{:?}", event);
     }
 
-    fn channel_msg(&mut self, irc: &Irc, channel: &Channel, user: &ChannelUser, msg: &str) {
+    fn channel_msg(&mut self, irc: Arc<Irc>, channel: Arc<Channel>, user: Arc<ChannelUser>, msg: &str) {
         if msg.starts_with(NICKNAME) {
-            println!("got highlighted");
+            self.blah += 1;
+            let resp = format!("blah {}", self.blah);
+            irc.privmsg("#clux", &resp); // channel.name is private so can't actually use it atm
+            thread::spawn(move || {
+                thread::sleep(std::time::Duration::from_secs(4));
+                irc.privmsg("#clux", "done");
+            });
         }
     }
 
-    fn reconnect(&mut self, irc: &Irc) {
+    fn reconnect(&mut self, irc: Arc<Irc>) {
         println!("reconnect");
     }
 
-    fn welcome(&mut self, irc: &Irc) {
+    fn welcome(&mut self, irc: Arc<Irc>) {
         irc.join("#clux", None);
     }
 }
@@ -38,8 +47,8 @@ fn main() {
         .realname(REALNAME)
         .reconnection(ReconnectionSettings::Reconnect {
             max_attempts: 0,
-            delay_between_attempts: Duration::seconds(5),
-            delay_after_disconnect: Duration::seconds(15),
+            delay_between_attempts: Duration::seconds(15),
+            delay_after_disconnect: Duration::seconds(30),
         })
-        .dispatch(Zedonius{ blah: true }).unwrap();
+        .dispatch(Zedonius{ blah: 0 }).unwrap();
 }
